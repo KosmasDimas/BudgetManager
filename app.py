9c58b983b9b026f802e59fae454b385220fadf77
KosmasDimas <dimaskosmas@gmail.com>
2023-10-14 12:48:26 +0300

    removed unused functions

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd, euro
+from helpers import apology, login_required, usd, euro
 
 # Configure application
 app = Flask(__name__)

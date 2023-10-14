c44264fe2f2d76cac2bde1c3d940d9d9b9865959
KosmasDimas <dimaskosmas@gmail.com>
2023-10-14 12:56:24 +0300

    db

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = sqlite3.connect("budget.db")
+db = sqlite3.connect("budget.db", check_same_thread=False)
 
 
 def select_recent_expenses(user_id, number):
